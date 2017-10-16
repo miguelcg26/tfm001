9651ca9baa6729dce4447f086d25e649c617b657
Jesús Salado Castillo <thefjs@gmail.com>
2017-10-16 16:55:11 +0200

    Sobreescrito el git completo, la versión 0.3.3

---
+++ Assets/Scripts/CursorLock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorLock : MonoBehaviour {
+
+	void Start () {
+		// Incialmente bloqueamos el cursor al cargar el objeto que tenga asignado este script
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update () {
+		// al pusar la tecla asignada al botón Cancel, liberamos el cursor
+		if (Input.GetButtonDown ("Cancel")) {
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+		// bloqueamos el cursor con el keyup de la tecla izquierda del ratón
+		if (Input.GetKeyUp (KeyCode.Mouse0)) {
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager GM;
+
+	public bool blockPlayer;
+
+	public bool charWalkingUp;
+
+	public bool charWalkingDown;
+	//[Header("Character")]
+	public enum CharPos { TOP, RIGHT_TOP, LEFT_TOP, BOTTOM, RIGHT_BOTTOM, LEFT_BOTTOM, VERTICAL }
+	public CharPos charPos;
+
+	public enum CharDir { RIGHT, LEFT } // LEFT es a la izq DEL personaje, no de lo que ves, same for RIGHT
+	public CharDir charDir;
+
+	//[Header("Camera")]
+	public enum CamPosX { BACK, FRONT, RIGHT, LEFT }
+	public CamPosX camPosX;
+
+	public enum CamPosY { NORMAL, UP, DOWN }
+	public CamPosY camPosY;
+
+	public enum CamPosZ { TOP, BOTTOM }
+	public CamPosZ camPosZ;
+
+
+	//public GameObject[] attractors;
+
+	//public GameObject currentAttractor;
+
+	void Awake () {
+		//DontDestroyOnLoad(transform.gameObject);
+
+
+		if (GM == null)
+			GM = this;	
+		else
+			Destroy (gameObject);
+
+	}
+
+	/*
+	public GameObject gameMenu;
+	public GameObject pauseMenu;
+
+
+
+	public void Pause() {
+		//pausa la escena
+		gameMenu.SetActive (false);
+		pauseMenu.SetActive (true);
+		Time.timeScale = 0;
+	}
+
+	public void Resume() {
+		//reanuda la escena
+		gameMenu.SetActive (true);
+		pauseMenu.SetActive (false);
+		Time.timeScale = 1;
+	}
+
+	public void Retry() {
+		//recarga la escena
+		Time.timeScale = 1;
+
+		pauseMenu.SetActive (false);
+		//Hacemos un Face In
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		//introGame = false;
+		//fadeCanvasGroup.alpha = 1f;
+
+	}
+
+	public void Quit() {
+		Application.Quit();
+	}
+
+	*/
+
+
+
+}
